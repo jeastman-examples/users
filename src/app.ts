38c69fa57c7c9c2e6b3f10573fbe9eb1faab6a4f
Jorge Eastman <jeastman@pop-os.localdomain>
2022-07-10 04:02:22 -0300

    refactor routers

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
 import createError from 'http-errors';
+import cors from 'cors';
 import express from 'express';
-
-// const usersRouter = require('./routes/users');
+import helmet from 'helmet';
+import morgan from 'morgan';
+import router from './routes';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use('/users', usersRouter);
+app.use(helmet());
+app.use(morgan('combined'));
+
+app.use('/', router);
+
 app.use((req, res, next) => {
     next(createError(404));
 });
+
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.send(err);
